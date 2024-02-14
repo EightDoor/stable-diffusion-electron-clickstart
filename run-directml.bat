6d2618f7f61ebc007d61cf708a3d995319d2a71a
eightdoor <851708184@qq.com>
2024-02-14 19:04:55 +0800

    fix

--- run-directml.bat
+++ run-directml.bat
@@ -7,6 +7,6 @@ cd %~dp0webui-directml
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--use-directml --update-check --update-all-extensions --reinstall-xformers --reinstall-torch
-
+set COMMANDLINE_ARGS=--opt-sub-quad-attention --medvram --disable-nan-check --use-directml --update-check --update-all-extensions --reinstall-xformers --reinstall-torch
+@REM 您可能不再需要 --lowvram 或 --medvram。
 call webui.bat
